87e8b12f2c7a5c8f9be6d37bc0aa73e048f18c37
He Xiang Koh <Xiang@corp.avensys.com>
2023-08-22 16:29:53 +0800

    Added prefilter to request context and post filter to append response

--- job-service/src/main/java/com/kohhx/jobservice/controller/JobController.java
+++ job-service/src/main/java/com/kohhx/jobservice/controller/JobController.java
@@ -1,14 +1,25 @@
 package com.kohhx.jobservice.controller;
 
+import com.kohhx.jobservice.DTO.MessageDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Map;
 
 @RestController
 //@RequestMapping("/job")
 public class JobController {
 
     @GetMapping("/get")
-    public String getJob() {
-        return "Job";
+    public ResponseEntity<MessageDTO> getJob(@RequestHeader Map<String,String> headers) {
+        headers.forEach((key,value) ->{
+            System.out.println("Header Name: "+key+" Header Value: "+value);
+        });
+        return new ResponseEntity<>(new MessageDTO("Got all jobs"), HttpStatus.OK);
     }
 }
